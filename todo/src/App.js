dcf107b17c6ee750032f281e8a33cc21a08cb365
Filipe <filipeccm@gmail.com>
2020-11-19 17:33:13 +0100

    todo skeleton done

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+//TodoList -> TodoItem
+//
+
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const onKeyDown = ({ target, keyCode }) => {
+    if (keyCode === 13) {
+      setNewTodo(target.value);
+      target.value = '';
+      setTodoList([...todoList, newTodo]);
+    }
+  };
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <input onKeyDown={onKeyDown} type="text" />
+      {todoList ? todoList.map((todo, idx) => <div key={idx}>{todo}</div>) : ''}
     </div>
   );
 }
